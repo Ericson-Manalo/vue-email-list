62f1e4cc488c794777cc9cddfc6c0aa5be3046f6
Ericson-Manalo <ericmana98@gmail.com>
2023-06-13 16:04:53 +0200

    created a for into the function including axios

--- js/myscript.js
+++ js/myscript.js
@@ -2,13 +2,33 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            mail:'',
 
 
         }
     },
     methods: {
-        
+
+        getRandomMail(){
+
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then ((response) => {
+                    const result = response.data;
+                    console.log(result.response);
     
+    
+                    this.mail = result.response;
+                });
+                
+            }
+
+
+        }
+    },
+
+    created(){
+        this.getRandomMail();
     }
 
 }).mount('#app');
